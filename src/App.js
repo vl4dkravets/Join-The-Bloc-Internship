a3c1aed935634005cd0a3065b0e1eebe40bad125
Eli Sultanov <elisultanov@Elis-MacBook-Pro.local>
2020-10-22 12:38:07 -0400

    files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1> Hello </h1>
+    </>
+  );
+}
+
+export default App;
