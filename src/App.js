f50c5eb9d455151df1bfe603674e669d0cd3423d
Juyin Chen <juyin512@gmail.com>
2020-10-24 18:09:03 -0400

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SearchForm from './Components/Form.js';
+import JobLists from './Components/JobLists'
+import axios from 'axios'
 
 function App() {
-  return (
-    <>
-      <h1> Hello </h1>
-    </>
-  );
-}
+  const [posts, setPosts] = useState([])
+  const [description, setDescription] = useState('')
+  const [location, setLocation] = useState('')
 
+  //description input handle
+  const handleDescriptionChange = (e) => {
+    setDescription(e.target.value);
+  }
+  
+  //location input handle
+  const handleLocationChange = (e) => {
+    setLocation(e.target.value);
+  }
+  
+  //submit button handle
+  const onSubmit = e => {
+    e.preventDefault();
+    getData();
+
+  }
+
+  //get data from github job API (fetching by description and location)
+  const url = `https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=${description}&location=${location}`
+  const getData = () => {
+    axios.get(url)
+      .then(res => {
+        console.log(res)
+        setPosts(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  return (
+    <div>
+    <SearchForm
+        description={description}
+        handleDescriptionChange={handleDescriptionChange}
+        location={location}
+        handleLocationChange={handleLocationChange}
+        onSubmit={onSubmit} />
+
+    {
+        posts.map((job) => <JobLists job={job} key={job.id} />)
+    }
+    </div>
+  )
+}
 export default App;
